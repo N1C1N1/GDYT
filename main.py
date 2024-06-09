05d16e1635966b7c0baa216c9e653eed43d74e03
N1C1 <116889092+N1C1N1@users.noreply.github.com>
2024-06-09 14:53:57 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,93 @@
+from flet import *
+from asyncio import run
+from aiohttp import ClientSession
+
+async def get(id: str | int = None):
+    async with ClientSession() as s:
+        async with s.get(f"https://gdbrowser.com/api/search/{id}") as response:
+            response = await response.json()
+            return response[0]
+
+def main(page: Page):
+    def playground_change(e: ControlEvent):
+        if page.client_storage.get("api") != None:
+
+            try:
+                playground.content.error_text = None
+                playground.content.value = playground.content.value.format(**page.client_storage.get("api"))
+            except:
+                playground.content.error_text = 'key not found'
+
+            playground.content.update()
+            
+    playground = Container(
+        content=TextField(
+            hint_text="playground",
+            expand=True,
+            min_lines=27,
+            multiline=True,
+            border_radius=7,
+            on_change=playground_change,
+            error_style=TextStyle(25)
+        ),
+        bgcolor='#686D76',
+        expand=3,
+        alignment=alignment.center,
+        border_radius=7,
+        padding=6
+    )
+
+    def get_api(e):
+        page.client_storage.set("id", idfield.value.strip())
+
+        try:
+            api = run(get(idfield.value.strip()))
+            page.client_storage.set("api", api)
+
+            idfield.error_text = None
+            idfield.update()
+
+            def paste(e: ControlEvent):
+                playground.content.value = playground.content.value + '{' + e.control.text + '} '
+                playground.content.update()
+                playground_change(None)
+
+            buttons.controls = [
+                TextButton(i, on_click=paste) for i in list(api.keys())
+            ]
+            buttons.update()
+        except:
+            idfield.error_text = "level not found"
+            idfield.update()
+
+    idfield = TextField(
+        label="id",
+        on_submit=get_api,
+        value=page.client_storage.get("id")
+    )
+
+    buttons = Row(
+        controls=[], 
+        expand=2, 
+        wrap=True, 
+        alignment=MainAxisAlignment.CENTER, 
+        vertical_alignment=CrossAxisAlignment.CENTER,
+        scroll=ScrollMode.ADAPTIVE
+    )
+
+    selecter = Container(
+        content=Column([
+            idfield,
+            buttons
+        ]),
+        bgcolor='#373A40',
+        expand=1,
+        border_radius=7,
+        padding=6
+    )
+
+    page.add(
+        Row([playground, selecter], expand=True)
+    )
+
+app(main)
